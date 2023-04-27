fe1b8ec83d5fbdbb3b7917b2086e737e70d69a35
WRadoslaw <r.wyszynski00@gmail.com>
2023-04-27 18:40:09 +0200

    Prettier

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,61 +1,57 @@
-import { BaseDotsamaWallet, Wallet } from '../index'
-import { InjectedWindow, MetadataDef } from '@polkadot/extension-inject/types'
-import supportedWallets from './supportedWallets'
 import { ApiPromise, ApiRx } from '@polkadot/api'
-import { firstValueFrom } from './utils'
-import { base64Encode } from '@polkadot/util-crypto'
+import { InjectedWindow, MetadataDef } from '@polkadot/extension-inject/types'
 import { getSpecTypes } from '@polkadot/types-known'
+import { base64Encode } from '@polkadot/util-crypto'
 
-const supportedWalletsNames = supportedWallets.map(
-	(wallet) => wallet.extensionName,
-)
+import supportedWallets from './supportedWallets'
+import { firstValueFrom } from './utils'
+
+import { BaseDotsamaWallet, Wallet } from '../index'
+
+const supportedWalletsNames = supportedWallets.map((wallet) => wallet.extensionName)
 
 export function getWallets(): Wallet[] {
-	return supportedWallets
+  return supportedWallets
 }
 
 export function getAllWallets() {
-	const unknownWallets = Object.keys(
-		(window as Window & InjectedWindow)?.injectedWeb3 ?? {},
-	)
-		.filter((name) => !supportedWalletsNames.includes(name))
-		.map((wallet) => new BaseDotsamaWallet({ extensionName: wallet }))
+  const unknownWallets = Object.keys((window as Window & InjectedWindow)?.injectedWeb3 ?? {})
+    .filter((name) => !supportedWalletsNames.includes(name))
+    .map((wallet) => new BaseDotsamaWallet({ extensionName: wallet }))
 
-	return [...supportedWallets, ...unknownWallets]
+  return [...supportedWallets, ...unknownWallets]
 }
 
 export function getWalletBySource(source: string): Wallet | undefined {
-	const supportedWallet = supportedWallets.find((wallet) => {
-		return wallet.extensionName === source
-	})
-	return supportedWallet ?? new BaseDotsamaWallet({ extensionName: source })
+  const supportedWallet = supportedWallets.find((wallet) => {
+    return wallet.extensionName === source
+  })
+  return supportedWallet ?? new BaseDotsamaWallet({ extensionName: source })
 }
 
 export function isWalletInstalled(source: string): boolean {
-	const wallet = getWalletBySource(source)
-	return wallet?.installed as boolean
+  const wallet = getWalletBySource(source)
+  return wallet?.installed as boolean
 }
 
-export const getPolkadotApiChainInfo = async (
-	api: ApiRx | ApiPromise,
-): Promise<MetadataDef> => {
-	const systemChain = (await firstValueFrom(api.rpc.system.chain())) as string
+export const getPolkadotApiChainInfo = async (api: ApiRx | ApiPromise): Promise<MetadataDef> => {
+  const systemChain = (await firstValueFrom(api.rpc.system.chain())) as string
 
-	return {
-		icon: 'beachball',
-		chainType: 'substrate',
-		chain: systemChain,
-		metaCalls: base64Encode(api.runtimeMetadata.asCallsOnly.toU8a()),
-		types: getSpecTypes(
-			api.registry,
-			systemChain,
-			api.runtimeVersion.specName,
-			api.runtimeVersion.specVersion,
-		) as unknown as Record<string, string>,
-		specVersion: api.runtimeVersion.specVersion.toNumber(),
-		ss58Format: api.registry.chainSS58 ?? 0,
-		tokenDecimals: api.registry.chainDecimals[0],
-		tokenSymbol: api.registry.chainTokens[0],
-		genesisHash: api.genesisHash.toHex(),
-	}
+  return {
+    icon: 'beachball',
+    chainType: 'substrate',
+    chain: systemChain,
+    metaCalls: base64Encode(api.runtimeMetadata.asCallsOnly.toU8a()),
+    types: getSpecTypes(
+      api.registry,
+      systemChain,
+      api.runtimeVersion.specName,
+      api.runtimeVersion.specVersion,
+    ) as unknown as Record<string, string>,
+    specVersion: api.runtimeVersion.specVersion.toNumber(),
+    ss58Format: api.registry.chainSS58 ?? 0,
+    tokenDecimals: api.registry.chainDecimals[0],
+    tokenSymbol: api.registry.chainTokens[0],
+    genesisHash: api.genesisHash.toHex(),
+  }
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,87 +1,73 @@
-import {
-	InjectedAccount,
-	InjectedAccountWithMeta,
-	MetadataDef,
-} from '@polkadot/extension-inject/types'
+import { InjectedAccount, InjectedAccountWithMeta, MetadataDef } from '@polkadot/extension-inject/types'
 
 import { WalletError } from '../index'
 
-export type SubscriptionFn = (
-	accounts: WalletAccount[] | undefined,
-) => void | Promise<void>
+export type SubscriptionFn = (accounts: WalletAccount[] | undefined) => void | Promise<void>
 
 export interface WalletLogoProps {
-	// Logo url
-	src: string
-	// Alt for the Logo url
-	alt: string
+  // Logo url
+  src: string
+  // Alt for the Logo url
+  alt: string
 }
 
 export interface WalletAccount extends InjectedAccount {
-	source: string
-	wallet?: Wallet
-	signer?: unknown
+  source: string
+  wallet?: Wallet
+  signer?: unknown
 }
 
 export interface WalletData {
-	// The name of the wallet extension. Should match `WalletAccount.source`
-	extensionName: string
-	// Display name for the wallet extension
-	title: string
-	// Message to display if wallet extension is not installed
-	noExtensionMessage?: string
-	// The URL to install the wallet extension
-	installUrl: string
-	// The wallet logo
-	logo: WalletLogoProps
+  // The name of the wallet extension. Should match `WalletAccount.source`
+  extensionName: string
+  // Display name for the wallet extension
+  title: string
+  // Message to display if wallet extension is not installed
+  noExtensionMessage?: string
+  // The URL to install the wallet extension
+  installUrl: string
+  // The wallet logo
+  logo: WalletLogoProps
 }
 
 interface WalletExtension {
-	installed: boolean | undefined
+  installed: boolean | undefined
 
-	// The raw extension object which will have everything a dapp developer needs.
-	// Refer to a specific wallet's extension documentation
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	extension: any
+  // The raw extension object which will have everything a dapp developer needs.
+  // Refer to a specific wallet's extension documentation
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  extension: any
 
-	// The raw signer object for convenience. Usually the implementer can derive this from the extension object.
-	// Refer to a specific wallet's extension documentation
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	signer: any
+  // The raw signer object for convenience. Usually the implementer can derive this from the extension object.
+  // Refer to a specific wallet's extension documentation
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  signer: any
 }
 
 interface Signer {
-	// Sign function
-	sign?: (address: string, payload: string) => unknown
+  // Sign function
+  sign?: (address: string, payload: string) => unknown
 
-	// Update metadata function
-	updateMetadata: (chainInfo: MetadataDef) => Promise<boolean>
+  // Update metadata function
+  updateMetadata: (chainInfo: MetadataDef) => Promise<boolean>
 }
 
 interface Connector {
-	enable: (dappName: string) => unknown
+  enable: (dappName: string) => unknown
 
-	// Get accounts function
-	getAccounts: (anyType?: boolean) => Promise<WalletAccount[]>
+  // Get accounts function
+  getAccounts: (anyType?: boolean) => Promise<WalletAccount[]>
 
-	// The subscribe to accounts function
-	subscribeAccounts: (callback: SubscriptionFn) => unknown
+  // The subscribe to accounts function
+  subscribeAccounts: (callback: SubscriptionFn) => unknown
 }
 
 interface WalletErrors {
-	transformError: (err: WalletError) => Error
+  transformError: (err: WalletError) => Error
 }
 
 interface Transformers {
-	walletAccountToInjectedAccountWithMeta: (
-		account: WalletAccount,
-	) => InjectedAccountWithMeta
+  walletAccountToInjectedAccountWithMeta: (account: WalletAccount) => InjectedAccountWithMeta
 }
 
-export interface Wallet
-	extends WalletData,
-		WalletExtension,
-		Connector,
-		Signer,
-		Transformers,
-		WalletErrors {}
+export interface Wallet extends WalletData, WalletExtension, Connector, Signer, Transformers, WalletErrors {}
